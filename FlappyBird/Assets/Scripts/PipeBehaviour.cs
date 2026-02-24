ab6265ef1aea8622690f0876238e849469898297
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2026-02-24 15:24:53 -0300

    Created Player and pipe Behaviour
    - Made Flappy bird Jump
    - Created pipe behaviour with movement to the left
    - Created Ground with collider

---
+++ FlappyBird/Assets/Scripts/PipeBehaviour.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class PipeBehaviour : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+}
